6c69919444286feb0e97cf9472eae61f0e316508
Amarpreet Singh <amarpreet.singh1985@gmail.com>
2019-03-08 16:34:17 -0800

    added HTTP methods

--- todo_project/todo/views.py
+++ todo_project/todo/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
-
+from django.views.decorators.csrf import csrf_exempt
+from django.core import serializers
+#from models import TodoItem
 # Create your views here.
 
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
-
+@csrf_exempt
 def index(request):
-    return HttpResponse("Hello, world. You're at the todo index.")
+    if(request.method == "GET"):
+        return HttpResponse("Hello, world. You're at the todo index.")
+    
+    if(request.method == "POST"):
+         return HttpResponse("POST")
+
+    if(request.method == "DELETE"):
+        return HttpResponse("DELETE")
