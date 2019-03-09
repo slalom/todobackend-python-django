bfb1882a81e9f0f264c7b8f486fcfa6996ca7d45
Amarpreet Singh <amarpreet.singh1985@gmail.com>
2019-03-08 16:46:38 -0800

    update POST request

--- todo_project/todo/views.py
+++ todo_project/todo/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.core import serializers
-#from models import TodoItem
+
 # Create your views here.
 
 from django.http import HttpResponse, JsonResponse
@@ -12,7 +12,8 @@ def index(request):
         return HttpResponse("Hello, world. You're at the todo index.")
     
     if(request.method == "POST"):
-         return HttpResponse("POST")
+        body = request.body
+        return HttpResponse(body)
 
     if(request.method == "DELETE"):
         return HttpResponse("DELETE")
